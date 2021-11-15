c0bd48951b80c301a442d4818ab96c7e12934b94
Tan Dev Zai <superjunior242@gmail.com>
2021-11-15 11:27:24 +0700

    add docker into posts services

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.use(cors());
 
 const posts = {};
